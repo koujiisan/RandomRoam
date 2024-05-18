eeb60a75f6e80d9ea50746834e3b7e9f940f2d82
koujiisan <ftltf.gircc.lmni.smai@gmail.com>
2024-05-18 14:24:20 +0900

    add Front Back

---
+++ randomroam/src/pages/index.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import React from 'react';
+import Home from '../components/Home';
+
+const IndexPage = () => {
+  return <Home />;
+};
+
+export default IndexPage;
